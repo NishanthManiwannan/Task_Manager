8e0bd54b40508a54f195aeb797fb48d39573cb2f
Nishanth <m.nishanth1397@gmail.com>
2021-09-12 22:37:21 +0530

    Task.js new file

---
+++ src/components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const task = [
+    {
+        
+    }
+] 
+const Task = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Task
